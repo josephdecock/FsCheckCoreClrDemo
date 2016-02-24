4035aac23f5015f4660a568739e685df887b418e
josephdecock <josephdecock@gmail.com>
2016-02-23 20:11:44 -0600

    Initial Commit
    This creates a basic "Point" class that represents two dimensional
    points, and expresses a few properties about them using FsCheck.
    Notably, the project targets the core clr.

---
+++ Point/Point.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Point
+{
+    public class Point : IEquatable<Point>
+    {
+        public readonly int x;
+        public readonly int y;
+
+        public Point(int x, int y)
+        {
+            this.x = x; this.y = y;
+        }
+
+        public static bool operator ==(Point p1, Point p2)
+        {
+            return p1?.x == p2?.x && p1.y == p2.y;
+        }
+        public static bool operator !=(Point p1, Point p2)
+        {
+            return !(p1 == p2);
+        }
+
+        public override int GetHashCode()
+        {
+            // This algorithm is from Jon Skeet's stack overflow answer about GetHashCode
+            const int p1 = 17;
+            const int p2 = 23;
+
+            var hashCode = p1;
+
+            hashCode = hashCode * p2 + x.GetHashCode();
+            hashCode = hashCode * p2 + y.GetHashCode();
+
+            return hashCode;
+        }
+
+        public override bool Equals(object other)
+        {
+            return this == (other as Point);
+        }
+
+        bool IEquatable<Point>.Equals(Point other)
+        {
+            return this == other;
+        }
+    }
+
+}
---
+++ Point/PointTests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+using FsCheck;
+
+namespace Point
+{
+    public class PointTests
+    {
+
+        [Fact]
+        public void IntentionallyBroken()
+        {
+            Prop.ForAll<int, int>((x, y) =>
+            {
+                var p1 = new Point(x + 1, y);
+                var p2 = new Point(x, y);
+                return p1 == p2;
+            }).QuickCheckThrowOnFailure();
+        }
+
+        [Fact]
+        public void SameCoordsShouldBeEqual()
+        {
+            Prop.ForAll<int, int>((x, y) =>
+            {
+                var p1 = new Point(x, y);
+                var p2 = new Point(x, y);
+                return p1 == p2;
+            }).QuickCheckThrowOnFailure();
+        }
+
+        [Fact]
+        public void PointsWithDifferentXCoordsShouldBeNotEqual()
+        {
+            Prop
+            .ForAll<int, int, int>((x1, x2, y) =>
+                new Func<bool>(() =>
+                {
+                    var p1 = new Point(x1, y);
+                    var p2 = new Point(x2, y);
+                    return p1 != p2;
+                })
+            .When(x1 != x2))
+            .QuickCheckThrowOnFailure();
+        }
+
+        [Fact]
+        public void PointsWithDifferentYCoordsShouldBeNotEqual()
+        {
+            Prop
+            .ForAll<int, int, int>((y1, y2, x) =>
+                new Func<bool>(() =>
+                {
+                    var p1 = new Point(x, y1);
+                    var p2 = new Point(x, y2);
+                    return p1 != p2;
+                })
+            .When(y1 != y2))
+            .QuickCheckThrowOnFailure();
+        }
+
+        [Fact]
+        public void NullPointShouldNotEqualNonNullPoint()
+        {
+            Prop.ForAll<Point>(p => p != null)
+                .QuickCheckThrowOnFailure();
+        }
+
+        [Fact]
+        public void EqualPointsUsedAsKeysInADictionaryShouldCollide()
+        {
+            Prop.ForAll<int, int>((x, y) =>
+                {
+                    var p1 = new Point(x, y);
+                    var p2 = new Point(x, y);
+
+                    var d = new Dictionary<Point, object>();
+                    d.Add(p1, new { });
+                    Assert.Throws<ArgumentException>(() => d.Add(p2, new { }));
+                })
+            .QuickCheckThrowOnFailure();
+        }
+    }
+}
